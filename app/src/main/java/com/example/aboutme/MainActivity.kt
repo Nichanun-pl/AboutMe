b7343438aeb2eb6adc95b235a32647f1798c2b8e
Nichanun <nichanun.pl@rmuti.ac.th>
2020-07-21 20:22:36 +0700

    Use data binding to eliminate findViewById()

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -9,7 +9,10 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
+
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
